4106032134e86cf90cf132fee791bf91bfd22647
souryuu1003 <souryuu1003@gmail.com>
2022-05-16 23:11:36 +0900

    20220516

--- Movie/DAO/MovieAccess.cs
+++ Movie/DAO/MovieAccess.cs
@@ -6,31 +6,20 @@ namespace movieaccess
 {
 	public class MovieAccess
 	{
-		MovieVO movieVO;
-
-		int maxLength = 1;
+		string listOfMovie = "select mv_KEY_NO, mv_MOVIE_NAME, mv_START_DATE, mv_END_DATE" +", mv_THEATER, mv_SCREEN_SESSION, mv_MOVIE_TYPE" +
+			", mv_MOVIE_START_TIME, mv_MOVIE_END_TIME, mv_SEAT, mv_RESERVED_SEAT, mv_AUDIENCE_NUBER, mv_TOTAL_AUDIENCE_NUMBER" + " from Movie order by mv_START_DATE desc";
+		string sesearchOfMovie = "select mv_KEY_NO, mv_MOVIE_NAME, mv_START_DATE, mv_END_DATE" + ", mv_THEATER, mv_SCREEN_SESSION, mv_MOVIE_TYPE" +
+			", mv_MOVIE_START_TIME, mv_MOVIE_END_TIME, mv_SEAT, mv_RESERVED_SEAT, mv_AUDIENCE_NUBER, mv_TOTAL_AUDIENCE_NUMBER" + " from Movie where mv_MOVIE_NAME = title";
+		string searchOfEmptySeat = "select mv_KEY_NO, mv_MOVIE_NAME, mv_START_DATE, mv_END_DATE" + ", mv_THEATER, mv_SCREEN_SESSION, mv_MOVIE_TYPE" +
+			", mv_MOVIE_START_TIME, mv_MOVIE_END_TIME, mv_SEAT, mv_RESERVED_SEAT, mv_AUDIENCE_NUBER, mv_TOTAL_AUDIENCE_NUMBER" + " from Movie where mv_RESERVED_SEAT = false";
+		string rankingOfMovie = "select mv_KEY_NO, mv_MOVIE_NAME, mv_START_DATE, mv_END_DATE" + ", mv_THEATER, mv_SCREEN_SESSION, mv_MOVIE_TYPE" +
+			", mv_MOVIE_START_TIME, mv_MOVIE_END_TIME, mv_SEAT, mv_RESERVED_SEAT, mv_AUDIENCE_NUBER, mv_TOTAL_AUDIENCE_NUMBER" + " from Movie order by mv_AUDIENCE_NUBER desc";
+		string listOfEndMovie = "select mv_KEY_NO, mv_MOVIE_NAME, mv_START_DATE, mv_END_DATE" + ", mv_THEATER, mv_SCREEN_SESSION, mv_MOVIE_TYPE" +
+			", mv_MOVIE_START_TIME, mv_MOVIE_END_TIME, mv_SEAT, mv_RESERVED_SEAT, mv_AUDIENCE_NUBER, mv_TOTAL_AUDIENCE_NUMBER" + " from Movie order by mv_END_DATE desc";
 
 		public MovieAccess()
 		{
 			
-		}
-
-		public List<MovieVO> listMovie()
-		{
-
-			List<MovieVO> movieList = new List<MovieVO>();
-			
-			for (int i = 0; i < maxLength; i++)
-            {
-				movieVO = new MovieVO();
-				
-
-				movieList.Add(movieVO);
-			}
-
-			
-			
-			return movieList;
 		}
 	}
 }
--- Movie/DAO/UserAccess.cs
+++ Movie/DAO/UserAccess.cs
@@ -6,6 +6,13 @@ namespace useraccess
 {
 	public class UserAccess
 	{
+		string tikkettingOfMovie = "update Movie set mv_RESERVED_SEAT = true where ur_KEY_NO";
+		string searchOfCouponUser = "";
+		string currentOfTikketting = "";
+		string listOfTikketting = "select ur_KEY_NO, ur_USER_NAME, ur_CONTACT_INFO, ur_REG_DATE, ur_USER_GRADE" +
+			", ur_MOVIE_NO, ur_THEATER, ur_SCREEN_SESSION, ur_MOVIE_TYPE" +
+			"ur_MOVIE_START_TIME, ur_MOVIE_END_TIME, ur_SEAT, ur_USE_COUPON from User where ur_SEAT != \"\"";
+
 		public UserAccess()
 		{
 			
