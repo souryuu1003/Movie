0859d892f82b63a5d40e1c4c998f29c7a5fd26ed
souryuu1003 <souryuu1003@gmail.com>
2022-06-03 17:26:15 +0900

    sql

--- Movie/DAO/MovieAccess.cs
+++ Movie/DAO/MovieAccess.cs
@@ -6,8 +6,25 @@ namespace movieaccess
 {
 	public class MovieAccess
 	{
-		string listOfMovie = "select mv_KEY_NO, mv_MOVIE_NAME, mv_START_DATE, mv_END_DATE" +", mv_THEATER, mv_SCREEN_SESSION, mv_MOVIE_TYPE" +
-			", mv_MOVIE_START_TIME, mv_MOVIE_END_TIME, mv_SEAT, mv_RESERVED_SEAT, mv_AUDIENCE_NUBER, mv_TOTAL_AUDIENCE_NUMBER" + " from Movie order by mv_START_DATE desc";
+		string insertOfMovieList = "insert into Movie (mv_KEY_NO, mv_MOVIE_NAME, mv_START_DATE, mv_END_DATE, mv_THEATER, mv_SCREEN_SESSION, mv_MOVIE_TYPE, mv_MOVIE_START_TIME"
+			+ ", mv_MOVIE_END_TIME, mv_SEAT, mv_RESERVED_SEAT, mv_AUDIENCE_NUBER, mv_TOTAL_AUDIENCE_NUMBER, mv_INSERT_DATE, mv_INSERT_USER, mv_UPDATE_DATE, mv_UPDATE_USER)"
+			+ "values (mv_KEY_NO = mv_KEY_NO" + "mv_MOVIE_NAME = mv_MOVIE_NAME" + "mv_START_DATE = mv_START_DATE" + "mv_THEATER = mv_THEATER" + "mv_SCREEN_SESSION = mv_SCREEN_SESSION"
+			+ "mv_MOVIE_TYPE = mv_MOVIE_TYPE" + "mv_MOVIE_START_TIME = mv_MOVIE_START_TIME" + "mv_MOVIE_END_TIME = mv_MOVIE_END_TIME" + "mv_SEAT = mv_SEAT" + "mv_RESERVED_SEAT = mv_RESERVED_SEAT"
+			+ "mv_AUDIENCE_NUBER = mv_AUDIENCE_NUBER" + "mv_TOTAL_AUDIENCE_NUMBER = mv_TOTAL_AUDIENCE_NUMBER" + "mv_INSERT_DATE = mv_INSERT_DATE" + "mv_INSERT_USER = mv_INSERT_USER"
+			+ "mv_UPDATE_DATE = mv_UPDATE_DATE" + "mv_UPDATE_USER = mv_UPDATE_USER)";
+
+		string selectOfMovieList = "select mv_KEY_NO, mv_MOVIE_NAME, mv_START_DATE, mv_END_DATE, mv_THEATER, mv_SCREEN_SESSION, mv_MOVIE_TYPE, mv_MOVIE_START_TIME"
+			+ ", mv_MOVIE_END_TIME, mv_SEAT, mv_RESERVED_SEAT, mv_AUDIENCE_NUBER, mv_TOTAL_AUDIENCE_NUMBER, mv_INSERT_DATE, mv_INSERT_USER, mv_UPDATE_DATE, mv_UPDATE_USER, mv_UPDATE_USER"
+			+ " from Movie order by mv_START_DATE desc";
+
+		string updateOfMovieList = "update Movie set"
+			+"mv_MOVIE_TYPE = mv_MOVIE_TYPE" + "and mv_MOVIE_START_TIME = mv_MOVIE_START_TIME" + "and mv_MOVIE_END_TIME = mv_MOVIE_END_TIME" + "and mv_SEAT = mv_SEAT"
+			+ "and mv_RESERVED_SEAT = mv_RESERVED_SEAT" + "and mv_AUDIENCE_NUBER = mv_AUDIENCE_NUBER" + "and mv_TOTAL_AUDIENCE_NUMBER = mv_TOTAL_AUDIENCE_NUMBER"
+			+ "and mv_UPDATE_DATE = mv_UPDATE_DATE" + "and mv_UPDATE_USER = mv_UPDATE_USER)";
+
+		string deleteOfMovie = "delete Movie where mv_KEY_NO = mv_KEY_NO";
+
+
 		string sesearchOfMovie = "select mv_KEY_NO, mv_MOVIE_NAME, mv_START_DATE, mv_END_DATE" + ", mv_THEATER, mv_SCREEN_SESSION, mv_MOVIE_TYPE" +
 			", mv_MOVIE_START_TIME, mv_MOVIE_END_TIME, mv_SEAT, mv_RESERVED_SEAT, mv_AUDIENCE_NUBER, mv_TOTAL_AUDIENCE_NUMBER" + " from Movie where mv_MOVIE_NAME = title";
 		string searchOfEmptySeat = "select mv_KEY_NO, mv_MOVIE_NAME, mv_START_DATE, mv_END_DATE" + ", mv_THEATER, mv_SCREEN_SESSION, mv_MOVIE_TYPE" +
--- Movie/DAO/UserAccess.cs
+++ Movie/DAO/UserAccess.cs
@@ -6,7 +6,26 @@ namespace useraccess
 {
 	public class UserAccess
 	{
-		string tikkettingOfMovie = "update Movie set mv_RESERVED_SEAT = true where ur_KEY_NO";
+		string insertOUserList = "insert into Movie (ur_KEY_NO, ur_USER_NAME, ur_CONTACT_INFO, ur_REG_DATE, ur_USER_GRADE, ur_MOVIE_NO, ur_THEATER, ur_SCREEN_SESSION"
+			+ ", ur_MOVIE_TYPE, ur_MOVIE_START_TIME, ur_MOVIE_END_TIME, ur_SEAT, ur_USE_COUPON, ur_INSERT_DATE, ur_INSERT_USER, ur_UPDATE_DATE, ur_UPDATE_USER)"
+			+ "values (ur_KEY_NO = ur_KEY_NO" + "ur_USER_NAME = ur_USER_NAME" + "ur_CONTACT_INFO = ur_CONTACT_INFO" + "ur_USER_GRADE = ur_USER_GRADE" + "ur_MOVIE_NO = ur_MOVIE_NO"
+			+ "ur_THEATER = ur_THEATER" + "ur_SCREEN_SESSION = ur_SCREEN_SESSION" + "ur_MOVIE_TYPE = ur_MOVIE_TYPE" + "ur_MOVIE_START_TIME = ur_MOVIE_START_TIME" + "ur_MOVIE_END_TIME = ur_MOVIE_END_TIME"
+			+ "ur_SEAT = ur_SEAT" + "ur_USE_COUPON = ur_USE_COUPON" + "ur_INSERT_DATE = ur_INSERT_DATE" + "ur_INSERT_USER = ur_INSERT_USER"
+			+ "ur_UPDATE_DATE = ur_UPDATE_DATE" + "ur_UPDATE_USER = ur_UPDATE_USER)";
+
+		string selectOfUserList = "select ur_KEY_NO, ur_USER_NAME, ur_CONTACT_INFO, ur_REG_DATE, ur_USER_GRADE, ur_MOVIE_NO, ur_THEATER, ur_SCREEN_SESSION"
+			+ ", ur_MOVIE_TYPE, ur_MOVIE_START_TIME, ur_MOVIE_END_TIME, ur_SEAT, ur_USE_COUPON, ur_INSERT_DATE, ur_INSERT_USER, ur_UPDATE_DATE, ur_UPDATE_USER, ur_UPDATE_USER"
+			+ " from Movie order by ur_CONTACT_INFO desc";
+
+		string updateOfUserList = "update Movie set"
+			+ "ur_THEATER = ur_THEATER" + "and ur_SCREEN_SESSION = ur_SCREEN_SESSION" + "and ur_MOVIE_TYPE = ur_MOVIE_TYPE" + "and ur_MOVIE_START_TIME = ur_MOVIE_START_TIME"
+			+ "and ur_MOVIE_END_TIME = ur_MOVIE_END_TIME" + "and ur_SEAT = ur_SEAT" + "and ur_USE_COUPON = ur_USE_COUPON"
+			+ "and ur_UPDATE_DATE = ur_UPDATE_DATE" + "and ur_UPDATE_USER = ur_UPDATE_USER)";
+
+		string deleteOfUser = "delete Movie where ur_KEY_NO = ur_KEY_NO";
+
+
+		string tikkettingOfMovie = "update Movie set ur_MOVIE_END_TIME = true where ur_KEY_NO";
 		string searchOfCouponUser = "";
 		string currentOfTikketting = "";
 		string listOfTikketting = "select ur_KEY_NO, ur_USER_NAME, ur_CONTACT_INFO, ur_REG_DATE, ur_USER_GRADE" +
